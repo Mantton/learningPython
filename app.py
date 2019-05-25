d834560501e4ed2bcd7aab08cbf1df8ee85827a2
Mantton <50926838+Mantton@users.noreply.github.com>
2019-05-25 16:24:31 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import random
+
+num = random.randint(1, 9)
+
+
+while True:
+    user = input("ENTER A NUM BETWEEN 1 & 9 ")
+    if user.lower() == "quit":
+        break
+
+    try:
+        if int(user) > num:
+            print("TOO HIGH")
+        elif int(user) < num:
+            print('TOO LOW')
+        elif int(user) == num:
+            print("YOU ARE CORRECT!")
+            break
+
+    except ValueError:
+        print("INVALID INPUT")
